03d8254eaebd5245ad8d2a4f3baad1825ad29ba7
858385135@qq.com <858385135@qq.com>
2016-01-12 23:00:02 +0800

    完成使用异步的方式去做同步的验证数据有效性函数

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mysql-model
 在mysql的基础上增加创建模型以及验证，仅此而已
+
+
+
+#validate_rule 添加方法
+
+
+在 validate_rule.js 中按照如下方式
+
+exports['rule_name'] = function (req,next){
+    //验证代码
+
+    next(); //此处注意，要在函数的出口显示的调用next，如果有多个出口所有出口都要调用，不然会验证出错。
+}
+
+
--- test.js
+++ test.js
@@ -2,50 +2,38 @@
  * Created by gg on 2016/1/10.
  */
 
-
-
+var createMysqlPool = require('./lib/mysql_pool');
 var model = require('./lib/model');
 var debug = require('debug')('test');
 
-var vip_model = new model();
+var vip_model = new model(createMysqlPool());
 
 vip_model.table = 'eb_vip';
 
 vip_model.rules['id'] =[
-  ['unique','id重复','none'],
+  ['unique','id重复','none']
 ];
 vip_model.rules['name'] =[
   ['unique','用户名重复','none'],
-  ['length' ,'长度不正确！','4,10'],
+  ['length' ,'长度不正确！','4,10']
 ];
 vip_model.rules['qq'] =[
   ['require','qq号码是必须的','none'],
   ['equal','两次不相等','pwd']
 ];
 
-vip_model.data['id'] = 2;
+vip_model.data['id'] = 1;
 vip_model.data['name'] ='123456';
 vip_model.data['account'] =1;
-vip_model.data['pwd'] =2222;
+vip_model.data['pwd'] =2;
 vip_model.data['qq'] =1;
 
 
-vip_model.add();
-
-//var aaa = {};
-//aaa.a =111;
-//var bbb= 222;
-//function test(a){
-//  debug(aaa);
-//  aaa.a = 333;
-//  aaa.b = 333;
-//}
-//
-//test =test.bind(null,aaa,bbb);
-//
-//test();
-//
-//debug(aaa);
+vip_model.validation().then(()=>{
+  debug(111);
+},(err)=>{
+  debug(err);
+});
 
 
 
