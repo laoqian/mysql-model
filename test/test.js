fa9a90d0ba05ca80165965683b051ab3b8f0cedf
858385135@qq.com <858385135@qq.com>
2016-02-24 12:55:36 +0800

    修改校验逻辑，使用es7_async方式。测试通过

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@
  * Created by gg on 2016/1/10.
  */
 
+require("babel-polyfill");
+require('babel-register')
 var createMysqlPool = require('./../addons/mysql_pool');
 var model = require('./vip-model');
 var debug = require('debug')('test');
@@ -16,7 +18,7 @@ vip.data['comm'] = '这是一个栏目的测试，谢谢使用';
 
 
 
-//vip.add();
+vip.add();
 //var i = 0
 //setInterval(()=>{
 //  vip.data['name'] ='我的栏目';
@@ -26,6 +28,6 @@ vip.data['comm'] = '这是一个栏目的测试，谢谢使用';
 
 
 
-vip.page('10,1').order('id desc').select((result)=>{
-  debug(result.rows);
-});
+//vip.page('10,1').order('id desc').select((result)=>{
+//  debug(result.rows);
+//});
